53ed5e1598cae5c59810b9e068a600a949177988
Justin Lam <justin.kh.lam@gmail.com>
2021-10-02 21:44:18 +0000

    add logic for 5, query 4

---
+++ 4_queries/5_avg_cohort_assistance_time.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name as name, avg(completed_at-started_at) as average_assistance_time
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts on cohorts.id = cohort_id
+GROUP by cohorts.name
+ORDER by average_assistance_time asc;
